21b4c0e1b61dd0fde548ba343fb434b6495d4d51
cjonesTL <cjones@timberlinkaustralia.com.au>
2025-09-15 16:35:34 -0700

    cleanup

--- netbox-plugin/netbox_zabbix/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import AwxInventoryView
-
-urlpatterns = [
-    path("awx-inventory/", AwxInventoryView.as_view(), name="awx-inventory"),
-]
--- netbox-plugin/netbox_zabbix/template_content.py
+++
@@ -1,8 +0,0 @@
-from netbox.plugins import PluginTemplateExtension
-
-
-class _NoOp(PluginTemplateExtension):
-    models = []
-
-    def right_page(self):
-        return ""
